062a1015423a53cbc608691890f0b39b42e6ca51
Conor <conor@johngregg.org>
2022-03-13 14:01:25 -0500

    Histogram displayed for each distribution

---
+++ src/app/distribution-chart/distribution-chart.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DistributionChartComponent } from './distribution-chart.component';
+
+describe('DistributionChartComponent', () => {
+  let component: DistributionChartComponent;
+  let fixture: ComponentFixture<DistributionChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DistributionChartComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DistributionChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/distribution-chart/distribution-chart.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Distribution } from "../distribution/distribution";
+import { Chart } from "chart.js";
+
+@Component({
+  selector: 'app-distribution-chart',
+  templateUrl: './distribution-chart.component.html',
+  styleUrls: ['./distribution-chart.component.css']
+})
+export class DistributionChartComponent implements OnInit {
+  @Input() distribution: Distribution | undefined;
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (this.distribution) {
+      const canvas: HTMLElement | null = document.getElementById("myChart");
+      // @ts-ignore
+      const context = canvas.getContext("2d");
+      new Chart(context, {
+        type: 'bar',
+        data: {
+          labels: this.distribution.getBucketLabels(),
+          datasets: [{
+            data: this.distribution.getBucketValues()
+          }]
+        },
+        options: {}
+      });
+    }
+  }
+
+}
